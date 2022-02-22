5c56d6c45552d80bf4e0656402f035fd45e4d888
koki-develop <kou.pg.0131@gmail.com>
2022-02-22 21:33:22 +0900

    eslintrc を更新

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,75 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
+  settings: {
+    react: {
+      version: 'detect',
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
+  },
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', 'import', 'unused-imports'],
+  rules: {
+    'react/prop-types': 'off',
+    semi: ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    quotes: ['error', 'single'],
+    'object-curly-spacing': ['error', 'always'],
+    'react/jsx-tag-spacing': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'object',
+          'type',
+          'index',
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+        pathGroups: [
+          {
+            pattern: '@/components/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+      },
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      settings: {
+        'import/resolver': {
+          typescript: {
+            alwaysTryTypes: true,
+            project: './',
+          },
         },
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+      },
+      extends: ['plugin:@typescript-eslint/recommended'],
+      plugins: ['@typescript-eslint'],
+      parser: '@typescript-eslint/parser',
+      rules: {},
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+  ],
+};
