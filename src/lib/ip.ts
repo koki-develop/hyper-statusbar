093f8d3ec61e56d620416c7315fa62300dc81ced
koki-develop <kou.pg.0131@gmail.com>
2022-07-13 08:38:28 +0900

    ip アドレスの取得に https://checkip.dev を使うようにした

--- src/lib/ip.ts
+++ src/lib/ip.ts
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
-const publicIpApiUrl = 'https://checkip.amazonaws.com/';
+const publicIpApiUrl = 'https://checkip.dev';
+
+type CheckIPResponse = {
+  source_ip: string;
+}
 
 export const fetchPublicIp = async (): Promise<string> => {
-  const { data } = await axios.get<string>(publicIpApiUrl);
-  return data;
+  const { data } = await axios.get<CheckIPResponse>(publicIpApiUrl);
+  return data.source_ip;
 };
